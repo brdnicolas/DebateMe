5d364b74f3a4713a37658a818b47a294b69c4280
brouar_n <brouar_n@etna-alternance.net>
2020-03-13 16:00:35 +0100

    Vierge comme Léo

---
+++ front/src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>TEST</p>
+    <button @clicl="getTest()">Refresh</button>
+    <ul>
+      <li v-for="test in tests" :key="test.id">{{test.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  created() {
+    this.getTest();
+  },
+  name: 'Test',
+  data() {
+    return {
+      errors: [],
+      tests: [],
+    };
+  },
+  methods: {
+    getTest() {
+      axios.get(
+        'http://localhost:5000/test',
+      ).then((response) => {
+        this.tests = response.data;
+      })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
