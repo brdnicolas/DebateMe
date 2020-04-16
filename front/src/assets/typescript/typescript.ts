d653054b23e49fbc9e80d80d1c5354debdacaee4
brouar_n <brouar_n@etna-alternance.net>
2020-04-16 10:36:42 +0200

    typescript et vueJSé

--- front/src/assets/typescript/typescript.ts
+++
@@ -1,5 +0,0 @@
-function greeter(name) {
-  return "Bonjour, " + name;
-}
-var myName = "Brouard Nicolas";
-document.body.innerHTML = greeter(myName);
--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -1,32 +1,40 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <Test/>
+    <p>
+      Nice
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
   </div>
 </template>
 
-<!--<script>
-import Test from './Test.vue';
-
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-  components: {
-    Test,
-  },
-};
-</script>-->
-
 <script lang="ts">
-  import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-  @Component
+@Component
 export default class HelloWorld extends Vue {
-  @Prop(Number) readonly propA: number | undefined;
-  @Prop({ default: 'default value' }) readonly propB!: string;
-  @Prop([String, Boolean]) readonly propC: string | boolean | undefined
+  @Prop() private msg!: string;
 }
 </script>
 
