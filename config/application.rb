6bff589f70c2714efd954a3eff6b50524a3839b1
Huugoo147 <hugo.vast@gmail.com>
2020-05-26 10:36:21 +0200

    CORS

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -33,5 +32,7 @@ module ApiDebateMe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+
   end
 end
