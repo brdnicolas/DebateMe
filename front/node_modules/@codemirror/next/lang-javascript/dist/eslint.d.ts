492e0c85a167ab4bf9031dc1d3503d359ef88442
Huugoo147 <hugo.vast@gmail.com>
2020-07-25 22:22:30 +0200

    update sans fornt

--- front/node_modules/@codemirror/next/lang-javascript/dist/eslint.d.ts
+++
@@ -1,3 +0,0 @@
-import { EditorView } from "../../view";
-export declare function esLint(eslint: any, config?: any): (view: EditorView) => any;
-//# sourceMappingURL=eslint.d.ts.map
--- front/node_modules/@codemirror/next/lang-javascript/dist/javascript.d.ts
+++
@@ -1,4 +0,0 @@
-import { LezerSyntax } from "../../syntax";
-export declare const javascriptSyntax: LezerSyntax;
-export declare function javascript(): import("../../state/dist").Extension;
-//# sourceMappingURL=javascript.d.ts.map
