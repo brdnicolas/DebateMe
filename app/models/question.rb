b4b5eb9f2526a5c0b2ffeb7e2e2e0a7baa9c6ce0
Huugoo147 <hugo.vast@gmail.com>
2020-07-27 08:57:15 +0200

    patch image user

--- app/models/question.rb
+++ app/models/question.rb
@@ -28,7 +28,7 @@ class Question < ApplicationRecord
   def posts_parsed
     new_posts = []
     posts.order(up: :desc).each do |element|
-      new_el = { user: element.user.user_info, post: element, votes_id: element.get_vote_ids }
+      new_el = { user: element.user.full_info, post: element, votes_id: element.get_vote_ids }
       new_posts.append(new_el)
     end
     new_posts
